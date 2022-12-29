d7410645af762036e0cc6ec82ce87a4edee4c61b
bnbrwd <56092771+bnbrwd@users.noreply.github.com>
2022-12-30 02:03:46 +0530

    Update strings.dart

--- rapido_bloc/lib/constant/strings.dart
+++ rapido_bloc/lib/constant/strings.dart
@@ -3,5 +3,5 @@ const OTP_SCREEN_ROUTE = '/otp_screen_data';
 const LOCATIONPERMISSION_GIVEN_ROUTE = '/locationpermissiongiven';
 const LOCATIONPERMISSION_NOT_GIVEN_ROUTE = '/locationpermissionnotgiven';
 
-const String LOGIN_URL = 'http://staging-api.rapido.bid/api/v1/login';
-const String VERIFY_URL = 'http://staging-api.rapido.bid/api/v1/verify';
+const String LOGIN_URL = 'http://';
+const String VERIFY_URL = 'http://';
